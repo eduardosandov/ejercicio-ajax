98e96f0b60d42df6c2932d8a14524a4cb27f104f
Eduardo Sandoval <44176744+eduardosandov@users.noreply.github.com>
2021-08-28 15:33:06 -0500

    Create funciones.php

---
+++ ejercicio2/funciones.php
@@ -0,0 +1,8 @@
+<?php
+$nom=$_POST['nombre'];
+$ciudad=$_POST['ciudad'];
+$ed=$_POST['edad'];
+$cel=$_POST['celular'];
+
+echo "Nombre: ".$nom. "\nCiudad: ".$ciudad. "\nEdad: ".$ed."\nCelular: ".$cel;
+?>
